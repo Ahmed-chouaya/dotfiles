e9135ad16c44927b96308a4422b1a7cb930022b8
Ahmed chouaya <chouaya.ahmed83@gmail.com>
2026-01-04 22:32:14 +0100

    Initial commit: flake + home-manager configs

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Milos + Caelestia setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs,home-manager, caelestia-shell, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations.ahmed =
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+        extraSpecialArgs = {
+          inherit caelestia-shell;
+        };
+      };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,39 @@
+{ pkgs, caelestia-shell, ... }:
+
+{
+  home.username = "ahmed";
+  home.homeDirectory = "/home/ahmed";
+  home.stateVersion = "25.11";
+
+
+  # Packages installed for the user
+  home.packages = with pkgs; [
+    # Caelestia Shell
+    caelestia-shell.packages.${pkgs.system}.default
+
+
+    # Runtime deps Caelestia expects
+    wl-clipboard
+    playerctl
+    brightnessctl
+    networkmanager
+    pipewire
+    wireplumber
+
+    # Personal Packages
+    brave
+    gh
+  ];
+
+  programs.git.enable = true;
+
+
+  # Optional: environment sanity
+  home.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    XDG_SESSION_TYPE = "wayland";
+  
+    # System theme variable
+    SYSTEM_THEME = "dark";
+  };
+}
