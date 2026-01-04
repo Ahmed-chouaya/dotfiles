bf5ba3c9054c57590e058f328a87a7d98943cedf
Ahmed Chouaya <chouaya.ahmed83@gmail.com>
2026-01-05 00:37:57 +0100

    Update Hyprland configuration paths in home.nix to use builtins.toPath and pkgs.lib.cleanSource for improved source management and clarity.

--- home.nix
+++ home.nix
@@ -72,8 +72,8 @@
   # Hyprland configuration
   # -------------------------------
   # Main Hyprland config file
-  home.file.".config/hypr/hyprland.conf".source = "./hypr/hyprland/hyprland.conf";
+  home.file.".config/hypr/hyprland.conf".source = builtins.toPath ./hypr/hyprland/hyprland.conf;
 
   # Modular files folder (env.conf, general.conf, etc.)
-  home.file.".config/hypr/hyprland/".source = "./hypr/hyprland";
+  home.file.".config/hypr/hyprland/".source = pkgs.lib.cleanSource ./hypr/hyprland;
 }
