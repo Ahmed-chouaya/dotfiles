9821f3221d9a9c0ee27fa88016b57b168d4283c6
Ahmed Chouaya <chouaya.ahmed83@gmail.com>
2026-01-04 23:40:31 +0100

    Refactored flake.nix to improve formatting and include caelestia-shell home module in home-manager configuration.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +15,7 @@
     };
   };
 
-  outputs = { self, nixpkgs,home-manager, caelestia-shell, ... }:
+  outputs = { self, nixpkgs, home-manager, caelestia-shell, ... }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -24,12 +23,11 @@
     homeConfigurations.ahmed =
       home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+
         modules = [
           ./home.nix
+          caelestia-shell.homeModule
         ];
-        extraSpecialArgs = {
-          inherit caelestia-shell;
-        };
       };
   };
 }
