f3d56a2d0a28709b9263e863a30c483e7e8da3be
Ahmed Chouaya <chouaya.ahmed83@gmail.com>
2026-01-04 23:18:30 +0100

    Organized package list in home.nix with section headers for better readability and maintenance.

--- home.nix
+++ home.nix
@@ -5,14 +5,16 @@
   home.homeDirectory = "/home/ahmed";
   home.stateVersion = "25.11";
 
-
   # Packages installed for the user
   home.packages = with pkgs; [
+    # --------------------------
     # Caelestia Shell
+    # --------------------------
     caelestia-shell.packages.${pkgs.system}.default
 
-
+    # --------------------------
     # Runtime deps Caelestia expects
+    # --------------------------
     wl-clipboard
     playerctl
     brightnessctl
@@ -20,19 +22,51 @@
     pipewire
     wireplumber
 
+    # --------------------------
     # Personal Packages
+    # --------------------------
     brave
     gh
+
+    # --------------------------
+    # Hyprland / Wayland dependencies
+    # --------------------------
+    hyprland
+    foot
+    vscode
+    thunar
+    wayland-protocols
+    polkit-gnome
+    libinput
+    xrandr       # optional for monitor config
+
+    # --------------------------
+    # Audio / Multimedia
+    # --------------------------
+    pamixer      # for volume keybinds
+    swaylock     # screen lock (or hyprland-lock if available)
+
+    # --------------------------
+    # Cursor / Theme / GTK+ Qt
+    # --------------------------
+    sweet-cursors
+    lxappearance
+    qt5ct
+    qt6ct
+
+    # --------------------------
+    # Optional extras
+    # --------------------------
+    # hyprpaper   # wallpaper manager if you want dynamic wallpapers
   ];
 
   programs.git.enable = true;
-
 
   # Optional: environment sanity
   home.sessionVariables = {
     QT_QPA_PLATFORM = "wayland";
     XDG_SESSION_TYPE = "wayland";
-  
+
     # System theme variable
     SYSTEM_THEME = "dark";
   };
